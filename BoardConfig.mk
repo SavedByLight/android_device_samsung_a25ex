9bdd017b10ff0ed31a0ba0135da75962c08db89b
Jamie Falconer <mrfluffyoven@gmail.com>
2025-12-24 11:51:19 +0000

    Update prebuilt kernel path in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
